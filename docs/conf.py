06647ef67d53d7966a401c408e4129cddf33b498
Benjamin Chamand <10047581+bchamand@users.noreply.github.com>
2020-07-09 17:23:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,8 @@ copyright = f"{datetime.datetime.now().year}, {author}."
 
 github_user = "bchamand"
 github_repo = project.lower()
+github_version = version
+git_revision = ""
 
 def subprocess_cmd(cmd):
     res = subprocess.run(
@@ -35,7 +37,6 @@ def subprocess_cmd(cmd):
     )
     return res.stdout.strip().decode("utf-8")
 
-github_version = version
 try:
     list_tags = subprocess_cmd("git tag --list --sort=-v:refname").split("\n")
     github_version = f"v{version}"
@@ -44,7 +45,6 @@ try:
 except:
     pass
 
-git_revision = ""
 try:
     git_revision = subprocess_cmd("git rev-parse --short HEAD")
 except:
