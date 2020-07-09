43a49c4a9dfc9a376c38533c83f45ba1b743d91c
Benjamin Chamand <10047581+bchamand@users.noreply.github.com>
2020-07-09 22:13:27 +0200

    Remove the custom css

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,6 @@ master_doc = "index"
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-#html_css_files = ["css/custom.css"]
 html_js_files = ["js/sphinx_rtd_versions.js"]
 
 html_context = {
