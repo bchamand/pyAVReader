f2f404e23fa6e47fe6b6ae193b6786dfe39f90c9
Benjamin Chamand <10047581+bchamand@users.noreply.github.com>
2020-07-09 12:25:35 +0200

    Remove the loading of get_file_info

--- avreader/__init__.py
+++ avreader/__init__.py
@@ -1,5 +1,4 @@
 from avreader.load import load, load_audio, load_video
-from avreader.utils import get_file_info
 from avreader.write import write, write_audio, write_video
 
 __version__ = "0.1.0-dev0"
